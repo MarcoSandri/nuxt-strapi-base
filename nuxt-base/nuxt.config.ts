856bc47859d7c646bc8c34ecd6b296361af8ca7d
Marco Sandri <marco.sandri@wearesim.it>
2023-02-23 15:42:31 +0100

    nuxt-base +scss +lenis +config

--- nuxt-base/nuxt.config.ts
+++ nuxt-base/nuxt.config.ts
@@ -1,4 +1,43 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
+  // * Load modules here
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxtjs/strapi'
+  ],
+
+  // * i18n configuration
+  i18n: {
+    strategy: 'prefix_except_default',
+    // * Locales in assets/i18n
+    locales: [
+      {
+        code: 'it',
+        file: 'it.json'
+      },
+    ],
+    langDir: 'assets/i18n',
+    // * Set up default locale code
+    defaultLocale: 'it',
+  },
+
+  // * Strapi configuration
+  strapi: {
+    url: process.env.API_ENDPOINT,
+    prefix: '/api',
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt',
+  },
+  css: ['@/assets/scss/resetscss.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/scss/main.scss";`,
+        }
+      }
+    }
+  }
 })
